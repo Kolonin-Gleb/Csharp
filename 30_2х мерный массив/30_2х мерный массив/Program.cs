e3a5c9157b16c444bf4413b5cd5b3f132a386f38
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-01-01 18:21:07 +0300

    Код уроков по массивам добавлен

---
+++ 30_2х мерный массив/30_2х мерный массив/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_2х_мерный_массив
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Способы инициализации 2х мерных массивов
+            // тип данных[,] имя массива
+            //int[,] arr = { {1, 3, 5, 7, 9},{0, 2, 4, 6, 8} };
+
+            //int[,] arr2 = new int[3, 5]; // 3 строки 5 столбцов
+
+            // В 2х мерном массиве кол. столбцов во всех строках должно быть одинаковым
+            int[,] myArray = new int[,]
+            {
+                {1, 3, 5, 7},
+                {2, 3, 5, 7},
+                {3, 3, 5, 7}
+            };
+
+            // Способы вывода 2х мерного массива
+
+            // Вывод в строку
+            //foreach (int i in myArray)
+            //{
+            //    Console.Write(i + " ");
+            //}
+
+            // Получение кол. эл. в заданном измерении
+            //Console.WriteLine(myArray.GetLength(0)); 
+
+            int height = myArray.GetLength(0);
+            int width = myArray.GetLength(1);
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write(myArray[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ 33_Зубчатые массивы/33_Зубчатые массивы/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _33_Зубчатые_массивы
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Зубчатый массив - 1м массив, что содержит в себе массивы,
+            // размеры которых могут быть разными
+
+            // Создание зубчатого массива
+            //int[][] myArray = new int[3][];
+
+            //// Создание 2х мерного прямоугольного массива
+            //int[,] myArray2 = new int[10, 2];
+
+            //// Ранг массива - кол. измерений массива
+            //int myArrayRank = myArray.Rank;
+            //int myArray2Rank = myArray2.Rank;
+
+            //Console.WriteLine("myArrayRank = " + myArrayRank);
+            //Console.WriteLine("myArray2Rank = " + myArray2Rank);
+
+            int[][] nums = new int[3][];
+            nums[0] = new int[2] { 1, 2 };          // выделяем память для первого подмассива
+            nums[1] = new int[3] { 1, 2, 3 };       // выделяем память для второго подмассива
+            nums[2] = new int[5] { 1, 2, 3, 4, 5 }; // выделяем память для третьего подмассива
+
+            // Используя вложенные циклы, можно перебирать зубчатые массивы
+
+            Console.WriteLine("Перебор с помощью цикла foreach");
+            foreach (int[] row in nums)
+            {
+                foreach (int num in row)
+                {
+                    Console.Write($"{num} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Перебор с помощью цикла for");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums[i].Length; j++)
+                {
+                    Console.Write($"{nums[i][j]} \t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
