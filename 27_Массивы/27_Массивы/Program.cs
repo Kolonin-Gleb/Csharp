71ea4636d586549c5f2f4952a55cdaf726f63b6e
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-01-01 17:22:37 +0300

    Добавлен код 2х уроков

---
+++ 27_Массивы/27_Массивы/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; // Для работы с массивами
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_Массивы
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 110, 10, 4, 10, 111, 64, 77, 4, 42, 5};
+
+            int min = arr.Min();
+
+            Console.WriteLine(min); // мин. эл.
+            Console.WriteLine(arr.Sum()); // сумма эл.
+
+            // Сумма чётных эл.
+            Console.WriteLine(arr.Where(i => i % 2 == 0).Sum()); // Лямбда выражения исп. для того, чтобы опр. чётный эл.
+
+            int[] unique_arr = arr.Distinct().ToArray(); // Получение уникальных эл. из массива
+            //Console.WriteLine(unique_arr);
+
+            Console.WriteLine("____________________");
+
+            // Сортировка массива
+            int[] sorted_arr = arr.OrderBy(i => i).ToArray();
+            Console.WriteLine(sorted_arr[0]);
+
+            sorted_arr = arr.OrderByDescending(i => i).ToArray();
+            Console.WriteLine(sorted_arr[0]);
+
+            Console.WriteLine("____________________");
+
+            // Сортировка массива через статические методы класса Array
+
+            Console.WriteLine(arr[0]);
+            Array.Sort(arr);
+            Console.WriteLine(arr[0]);
+
+            // Поиск в массиве
+
+            Console.WriteLine("____________________");
+
+            int[] chaosArr = { 111, 70, 10, 4, 64 };
+
+            int less70 = Array.Find(chaosArr, i => i < 70); // Поиск 1-ого числа в массиве < 70
+
+            Console.WriteLine(less70);
+
+            Console.WriteLine("____________________");
+
+            int[] arr2 = { 110, 10, 4, 10, 111, 64, 77, 4, 42, 5};
+
+            int result = Array.FindIndex(arr2, i => i == 78); // Поиск индекса эл. по условию
+            // Возращает -1, если эл. по условию не найден
+            Console.WriteLine(result);
+
+            Array.Reverse(arr2);
+
+        }
+    }
+}
---
+++ 28_Индексы и диапозоны/28_Индексы и диапозоны/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] myArray = { 2, 10, 5, 6, 77, 89, 101 };
+
+            //// Вывод последнего эл. массива
+            //Console.WriteLine(myArray[myArray.Length - 1]);
+            //// Сахар. Для его использование проект - .Net Core
+
+            //Console.WriteLine(myArray[^1]); // Нумерация с конца массива начинается с 1
+
+
+            //// Принцип работы диапозонов
+            //// Извлечение эл. из массива по диапозону
+
+            //int[] arr = myArray[1..4]; // начало диапозона..конец не вкл
+
+            //int[] myArray = { 2, 10, 5, 6, 77, 89, 101 };
+
+            //Index myIndex = ^2; // Индекс - это тоже тип данных (структура)
+            //// наличие ^ - взятие эл. с конца
+            //Console.WriteLine("Вывод данных из массива по индексу");
+            //Console.WriteLine(myArray[myIndex]);
+
+            //// Вывод полей структуры Index
+            //Console.WriteLine("Информация об использованном индексе");
+            //Console.WriteLine($"value {myIndex.Value} isFromEnd {myIndex.IsFromEnd}");
+
+
+            //// Доп. способ создания индексов
+
+            //int[] myArray = { 2, 10, 5, 6, 77, 89, 101 };
+
+            //Index index = new Index(3, true); // Перегрузка конструктора Index
+
+            //Console.WriteLine(myArray[index]);
+
+            //// Range - диапозон - тоже явл. структурой
+
+            //Range range = 1..4; // Эквивалент Range range = new Range(1, 4)
+
+            //Console.WriteLine(myArray[range]);
+
+            // Использование индексов и диапозонов к строке (ведь она тоже массив, только символов)
+            string str = "Hello World!!! =)";
+
+            Console.WriteLine(str);
+
+        }
+    }
+}
