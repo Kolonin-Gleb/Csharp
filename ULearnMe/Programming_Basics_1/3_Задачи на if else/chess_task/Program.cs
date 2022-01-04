09f6b3aef874a650ff3873faf16db0017206d2e5
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-01-04 14:43:22 +0300

    Исправлены баги

--- ULearnMe/Programming_Basics_1/3_Задачи на if else/chess_task/Program.cs
+++ ULearnMe/Programming_Basics_1/3_Задачи на if else/chess_task/Program.cs
@@ -6,9 +6,10 @@ using System.Threading.Tasks;
 
 namespace chess_task
 {
-    internal class Program
+    class Program
     {
-        enum Figures // Объявление перечисления
+        // Объявление перечисления
+        enum Figures 
         {
             Pawn = 1,
             Castle = 2,
@@ -17,11 +18,16 @@ namespace chess_task
             Queen = 5,
             King = 6,
         }
+        enum FiguresColors
+        {
+            White = 1,
+            Black = 2,
+        }
 
         /// <summary>
         /// Выводит информацию из Enum Figures в консоль.
         /// </summary>
-        static void PrintFiguresInfo()
+        static void PrintFiguresInfo() // TODO: Можно ли сделать так, чтобы этот метод работал с лююыми enum?
         {
             // Enum.GetVales() - возращает экземпляр System.Array, при этом каждому эл. массива
             // будет соответствовать член указанного перечисления.
@@ -31,7 +37,6 @@ namespace chess_task
 
             for (int i = 0; i < arr.Length; i++)
             {
-                // TODO: Как это работает?
                 Console.WriteLine("Фигура: {0}, номер для выбора = {0:D}", arr.GetValue(i));
             }
 
@@ -51,32 +56,33 @@ namespace chess_task
             Console.Write("Введите число соответствующее фигуре, которой производится ход: ");
             int figure = int.Parse(Console.ReadLine());
 
+            // Нужно также органиизовать ввод цвета фигуры, т.к. 
+            // чёрные и белые фигуры имеют разные возможности для хода.
+
             Console.Write("Введите клетку фигуры в формате е4: ");
             string start_position = Console.ReadLine();
 
             Console.Write("Введите клетку на которую хотите сходить в формате е4: ");
             string end_position = Console.ReadLine();
 
-            // Почему моё решение является антипаттерном?
-            // https://habr.com/ru/post/312792/
-            switch (figure)
+            switch ((int)figure)
             {
-                case Figures.Pawn:
+                case (int)Figures.Pawn:
                     Console.WriteLine("Pawn");
                     break;
-                case Figures.Castle:
+                case (int)Figures.Castle:
                     Console.WriteLine("Castle");
                     break;
-                case Figures.Horse:
+                case (int)Figures.Horse:
                     Console.WriteLine("Horse");
                     break;
-                case Figures.Bishop:
+                case (int)Figures.Bishop:
                     Console.WriteLine("Bishop");
                     break;
-                case Figures.Queen:
+                case (int)Figures.Queen:
                     Console.WriteLine("Queen");
                     break;
-                case Figures.King:
+                case (int)Figures.King:
                     Console.WriteLine("King");
                     break;
                 default:
@@ -86,7 +92,6 @@ namespace chess_task
 
             Console.WriteLine("Нажмите любую клавишу для завершения");
             Console.ReadKey();
-
         }
     }
 }
