3e8fcfc62d0d2c0cf1811f3dc86767db9853c1be
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-01-04 10:24:26 +0300

    Практика
    Решена 1 задача на ветвления.
    Начал решать задачу с шахматной доской.

---
+++ ULearnMe/Programming_Basics_1/3_Задачи на if else/3_Задачи на if else/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Задачи_на_if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Введите кол. рублей: ");
+                int rubles = int.Parse(Console.ReadLine());
+                HowManyRubles(rubles);
+            }
+        }
+        static void HowManyRubles(int rubles)
+        {
+            int shortenRubles = rubles;
+            shortenRubles %= 100;
+
+            while (true)
+            {
+                if (shortenRubles >= 5 && shortenRubles <= 20)
+                {
+                    Console.WriteLine(rubles + " рублей");
+                    break;
+                }
+                else
+                {
+                    shortenRubles %= 10;
+                    if (shortenRubles == 1) // Особый случай для 1
+                    {
+                        Console.WriteLine("1 рубль");
+                        break;
+                    }
+                    if (shortenRubles >= 2 && shortenRubles <= 4)
+                    {
+                        Console.WriteLine(rubles + " рубля");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine(rubles + " рублей");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ULearnMe/Programming_Basics_1/3_Задачи на if else/chess_task/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess_task
+{
+    internal class Program
+    {
+        enum Figures // Объявление перечисления
+        {
+            Pawn = 1,
+            Castle = 2,
+            Horse = 3,
+            Bishop = 4,
+            Queen = 5,
+            King = 6,
+        }
+
+        /// <summary>
+        /// Выводит информацию из Enum Figures в консоль.
+        /// </summary>
+        static void PrintFiguresInfo()
+        {
+            // Enum.GetVales() - возращает экземпляр System.Array, при этом каждому эл. массива
+            // будет соответствовать член указанного перечисления.
+
+            // Сохранение эл. перечисления в массив
+            Array arr = Enum.GetValues(typeof(Figures));
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                // TODO: Как это работает?
+                Console.WriteLine("Фигура: {0}, номер для выбора = {0:D}", arr.GetValue(i));
+            }
+
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Проверка возможности шахматного хода на пустой доске";
+
+            Console.WriteLine("\tПроверка возможности шахматного хода на пустой доске\n");
+
+            Console.WriteLine("Фигуры на выбор:");
+            PrintFiguresInfo();
+
+
+            Console.Write("Введите число соответствующее фигуре, которой производится ход: ");
+            int figure = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите клетку фигуры в формате е4: ");
+            string start_position = Console.ReadLine();
+
+            Console.Write("Введите клетку на которую хотите сходить в формате е4: ");
+            string end_position = Console.ReadLine();
+
+            // Почему моё решение является антипаттерном?
+            // https://habr.com/ru/post/312792/
+            switch (figure)
+            {
+                case Figures.Pawn:
+                    Console.WriteLine("Pawn");
+                    break;
+                case Figures.Castle:
+                    Console.WriteLine("Castle");
+                    break;
+                case Figures.Horse:
+                    Console.WriteLine("Horse");
+                    break;
+                case Figures.Bishop:
+                    Console.WriteLine("Bishop");
+                    break;
+                case Figures.Queen:
+                    Console.WriteLine("Queen");
+                    break;
+                case Figures.King:
+                    Console.WriteLine("King");
+                    break;
+                default:
+                    Console.WriteLine("Error!");
+                    break;
+            }
+
+            Console.WriteLine("Нажмите любую клавишу для завершения");
+            Console.ReadKey();
+
+        }
+    }
+}
