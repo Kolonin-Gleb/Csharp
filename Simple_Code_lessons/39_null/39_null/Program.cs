78c726726c0cb15cbb118b8bd561798af25b9e33
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-01-05 20:25:44 +0300

    Добавлен код 2х уроков

---
+++ Simple_Code_lessons/39_null/39_null/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _39_null
+{
+    class MyClass
+    {
+
+    }
+
+    struct myStruct
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool b;
+
+            //int a;
+
+            //double d;
+
+            //Random r;
+
+            //int[] arr;
+
+            //MyClass myClass;
+
+            //myStruct myStruct;
+            //// По умолч. у значимых типов (структур) значение не null
+            //// ПО умолч. у ссылочных типов (классов) значение null
+            //// null - пустая ссылка
+        }
+    }
+}
---
+++ Simple_Code_lessons/40_Оператор объединения с null/40_Оператор объединения с null/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _40_Оператор_объединения_с_null
+{
+    internal class Program
+    {
+        // Оператор null объединения ??
+        // Позволяет устанавливать null значения для ссылочных типов (классов)
+        // Синтаксический сахар
+        static void Main(string[] args)
+        {
+            string str = "text"; // = null - другой результат
+
+            // Проверка на то, что данные в str есть.
+            // Если их нет, то вывод данных по умолч.
+            Console.WriteLine(str ?? "нет текстовых данных");
+
+            //int num = null;
+
+            //Console.WriteLine(num ?? 0);
+
+            // 
+        }
+    }
+}
