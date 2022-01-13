a67095dd7f945860b1873ba6f5bac61c1b3e0f96
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-01-13 15:14:48 +0300

    Добавлена папка для кода из колледжа
    Добавлена папка для кода из колледжа.
    В этой папке будут храниться мои работы для колледжа.
    Приступил к выполению 1 практической работы.

---
+++ College/DemoSplitFiles/DemoSplitFiles/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DemoSplitFiles
+{
+    class Program
+    {
+        // Поскольку все классы объявлены в пространстве имен DemoSplitFiles, то для использования
+        // имен этих классов не нужно указывать дополнительных уточнений или иных действий. 
+        static void Main(string[] args)
+        {
+            // 1. Использование экземпляра класса Human
+            Human human = new Human("John", "Johnson", false);
+            human.Print();
+
+            // 2. Использование экземпляра класса Citizen
+            Citizen citizen = new Citizen("Petr", "Petrenko", false, "A101ksdl233", "New City");
+            Console.WriteLine("------------------------");
+            citizen.Print(); // вывести значение полей класса
+
+            // 3. Использование экземпляра класса ForeignCitizen
+            ForeignCitizen foreignCitizen = new ForeignCitizen("Ivanov", "Ivan", false,
+                "0230902ADK", "London", "023329032JJK",
+                Convert.ToDateTime("02.02.2020"), Convert.ToDateTime("03.03.2085"));
+            Console.WriteLine("------------------------");
+            foreignCitizen.Print();
+        }
+    }
+}
+
